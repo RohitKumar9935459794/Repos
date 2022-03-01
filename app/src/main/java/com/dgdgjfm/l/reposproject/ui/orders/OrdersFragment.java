9a8c178fbea5c8cb561f7dc82694d06d5eae798d
DELL <rohit307507@gmail.com>
2022-03-01 11:44:20 +0530

    projet

--- app/src/main/java/com/dgdgjfm/l/reposproject/ui/orders/OrdersFragment.java
+++ app/src/main/java/com/dgdgjfm/l/reposproject/ui/orders/OrdersFragment.java
@@ -2,6 +2,7 @@ package com.dgdgjfm.l.reposproject.ui.orders;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.transition.TransitionInflater;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,10 +11,12 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.dgdgjfm.l.reposproject.Add_AssetFragment;
+import com.dgdgjfm.l.reposproject.R;
 import com.dgdgjfm.l.reposproject.databinding.FragmentOrdersBinding;
 
 
@@ -42,6 +45,8 @@ public class OrdersFragment extends Fragment {
         binding.buttonAddAsset1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+               OrdersFragment ordersFragment=new OrdersFragment();
+
 
             }
         });
