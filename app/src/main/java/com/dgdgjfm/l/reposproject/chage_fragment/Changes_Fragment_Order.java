9c07cfea363d480b50322f9c46a4098cad23aa05
DELL <rohit307507@gmail.com>
2022-03-15 10:37:09 +0530

    projet

--- app/src/main/java/com/dgdgjfm/l/reposproject/chage_fragment/Changes_Fragment_Order.java
+++ app/src/main/java/com/dgdgjfm/l/reposproject/chage_fragment/Changes_Fragment_Order.java
@@ -8,6 +8,7 @@ import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.RequestQueue;
 import com.dgdgjfm.l.reposproject.R;
 import com.dgdgjfm.l.reposproject.databinding.FragmentChangeOrderBinding;
 
@@ -17,19 +18,28 @@ public class Changes_Fragment_Order extends Fragment {
      ListView listView;
 
      String[]  pump={"Ranchi",};
+    RequestQueue requestQueue;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        
-
-
         return inflater.inflate(R.layout.fragment_change_order, container, false);
-       /* ArrayAdapter<String> adapter= new ArrayAdapter<String>
-                (Changes_Fragment_Order.this, android.R.layout.simple_expandable_list_item_1);
-        // Using volley here
-        // listView.    */
+
+        // Using volley here for entering
+     /*  requestQueue= Volley.newRequestQueue(this);
+        JsonArrayRequest jsonArrayRequest= new JsonArrayRequest(Request.Method.GET, "https://jsonplaceholder.typicode.com/todos/1"
+                , null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+            Log.d("my app","the reponse is"+response.getString("Pritrol"));
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(" myapp"," Sommewthing went wrong");
+            }
+        });   */
 
     }
 }
