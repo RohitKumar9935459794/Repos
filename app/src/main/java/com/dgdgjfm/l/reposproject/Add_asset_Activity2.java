86cf863f204a0ed3bd81b48adc2becd851b307df
DELL <rohit307507@gmail.com>
2022-03-29 17:02:58 +0530

    projet

--- app/src/main/java/com/dgdgjfm/l/reposproject/Add_asset_Activity2.java
+++ app/src/main/java/com/dgdgjfm/l/reposproject/Add_asset_Activity2.java
@@ -3,24 +3,36 @@ package com.dgdgjfm.l.reposproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dgdgjfm.l.reposproject.databinding.ActivityAddAsset2Binding;
+import com.dgdgjfm.l.reposproject.model.Add_Asset;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Add_asset_Activity2 extends AppCompatActivity {
+
      ActivityAddAsset2Binding binding;
+     FirebaseDatabase database;
+     private String location;
+     private String Card;
+    private String Id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityAddAsset2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         getSupportActionBar().hide();
+        database=FirebaseDatabase.getInstance();
+        Id=getIntent().getStringExtra("ID") ;
 
         binding.buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                Add_Asset add_asset= new Add_Asset(binding.assetCapacity.getText().toString(),binding.assetName.getText().toString()) ;
+                  database.getReference("Asset").setValue(add_asset);
             }
         });
 
@@ -31,11 +43,71 @@ public class Add_asset_Activity2 extends AppCompatActivity {
               }
           });
 
-          
+          binding.buttonC1.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  binding.assetCapacity.setText("100");
+              }
+          });
+        binding.buttonC2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.assetCapacity.setText("400");
+            }
+        });
+        binding.buttonC3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.assetCapacity.setText("200");
+            }
+        });
+        binding.buttonC4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.assetCapacity.setText("600");
+            }
+        });
+        binding.buttonC5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.assetCapacity.setText("300");
+            }
+        });
+        binding.buttonC6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.assetCapacity.setText("800");
+            }
+        });
 
 
+        binding.djCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+                Toast.makeText(Add_asset_Activity2.this, "You selected Dj set", Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.industryCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Add_asset_Activity2.this, "You selected Industry", Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        binding.infraCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Add_asset_Activity2.this, "You selected Inftra structure", Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        binding.heavyCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Add_asset_Activity2.this, "You selected Heavy vechail", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
--- app/src/main/java/com/dgdgjfm/l/reposproject/model/Add_Asset.java
+++ app/src/main/java/com/dgdgjfm/l/reposproject/model/Add_Asset.java
@@ -1,4 +1,23 @@
 package com.dgdgjfm.l.reposproject.model;
 
 public class Add_Asset {
+    String Asset_Capacity,Asset_Name,card,location;
+
+    public Add_Asset() {
+    }
+
+    public Add_Asset(String Asset_Capacity,String Asset_Name, String Dj_card, String location){
+        this.Asset_Capacity=Asset_Capacity;
+        this.Asset_Name=Asset_Name;
+        this.card=Dj_card;
+        this.location=location;
+    }
+
+    public Add_Asset(String Asset_Capacity, String Asset_name){
+        this.Asset_Name=Asset_name;
+        this.Asset_Capacity=Asset_Capacity;
+    }
+
+
+
 }
